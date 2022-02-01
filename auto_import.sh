c6b872be8d159a88b864352eb69d0f67185161b6
JC Clark <jc.clark@modahealth.com>
2022-02-01 11:11:26 -0800

    auto_import script

---
+++ auto_import.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+
+export PATH=/home/db2inst1/cmsbin:$PATH
+export DATAMOUNT=/datadomain/export/
+
+
+cd $DATAMOUNT/icmnlsdb/look
+varchar.py icmnlsdb.Look.sql
+ 
+ cd $DATAMOUNT/odsutils/look
+ varchar.py odsutils.Look.sql
+
+
+
+new_migrate.sh
