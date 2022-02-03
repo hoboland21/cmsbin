44c29f22d7e381702233bb8eba8923892f74a9c1
hoboland21 <jc@saipantech.com>
2022-02-03 10:11:07 -0800

    Updated auto_export

--- auto_export.sh
+++ auto_export.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
+. /home/db2inst1/.profile
 
-cd /db/export
-### Setting up the mount for holding the database data  
+cd /datadomain/export
+
+rm -fr /datadomain/export/*
+
+echo $(date) Removed contents of the export directory
+
+### Setting up the mount for holding the database data
 export DATAMOUNT=$(pwd)
 ### Go over to the script directory
-XFR_START="$(date)"
 echo $(date) Kicking off the job
-exit
 cd /home/db2inst1/cmsbin
 ./new_export.sh icmnlsdb 819
 echo $(date) ICMNLSDB finished processing
+
 ./new_export.sh rmdb 1208
 echo $(date) RMDB finished processing
-./new_export.sh odsutils 819 
+
+./new_export.sh odsutils 819
 echo $(date) ODSUTILS finished processing
-rm -fr /datadomain/export/*
-echo $(date) Removed contents of the export directory
-mv /db/export/*    /datadomain/export 
-echo $(date) DB export contents have been moved to /datadomain
+
+echo $(date) Export from DB2 has finished
+echo ====================================
