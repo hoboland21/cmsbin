d91e9c2e943b3e32da2aa9590ba4df78e0341bc2
JC Clark <jc.clark@modahealth.com>
2022-02-03 10:03:40 -0800

    fixing the import

--- auto_import.sh
+++ auto_import.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+echo $(date) Beginning Import of Mensa Data set
+
 
 
 export PATH=/home/db2inst1/cmsbin:$PATH
 export DATAMOUNT=/datadomain/export/
 
-#
-#
-
-
-
-#cd $DATAMOUNT/icmnlsdb/look
-#varchar.py icmnlsdb.Look.sql
+cd $DATAMOUNT 
+echo $(date) creating backup data image
+rm -fr '/datadomain/export-gold/*'
+cp -r * '/datadomain/export-gold/'
+echo $(date) Running varchar.py on icmnmlsdb and odsutils
+cd $DATAMOUNT/icmnlsdb/look
+varchar.py icmnlsdb.Look.sql
  
-# cd $DATAMOUNT/odsutils/look
-# varchar.py odsutils.Look.sql
+cd $DATAMOUNT/odsutils/look
+varchar.py odsutils.Look.sql
 
 
+echo $(date) Executing new_migrate.sh 
 
-# new_migrate.sh
+new_migrate.sh
 
+echo $(date) Executing final_phase.sh 
 
 final_phase.sh
+
+echo $(date) Import complete - please run Content Manager configuration.
